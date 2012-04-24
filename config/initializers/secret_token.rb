f0437e84eabd5e1621aeb997202e590d769a6b4a
Joseph Barrozo <JABarrozO@gmail.com>
2012-04-24 10:08:46 +0800

    beri pirst push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaBAYANIhan::Application.config.secret_token = '12257d93854e0f34befddb2a3cb5b348a2abbc087bc5e46e592366dfffb4bc185bcda6b5d09bd2d1936519e66923c3772166bdbc0269ca06ddcd51b4dab1e6bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+KaBAYANIhan::Application.config.session_store :cookie_store, key: '_kaBAYANIhan_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# KaBAYANIhan::Application.config.session_store :active_record_store
